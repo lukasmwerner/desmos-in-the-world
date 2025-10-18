ed8868f1e58465e3eea5776859af4a5edd02256a
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-10-18 16:38:42 -0600

    Fix saving homography

--- homography/test_homography.py
+++ homography/test_homography.py
@@ -2,6 +2,8 @@ import pickle
 import cv2
 import os
 from dotenv import load_dotenv
+from screeninfo import get_monitors
+import numpy as np
 
 load_dotenv("../.env")
 
@@ -15,13 +17,13 @@ aruco_detector = cv2.aruco.ArucoDetector(aruco_dict)
 monitor = get_monitors()[0]
 monitor_frame = np.zeros((monitor.height, monitor.width, 3), dtype=np.uint8)
 
-source = cv2.VideoCapture(os.getenv("WEBCAM_ID"))
+source = cv2.VideoCapture(int(os.getenv("WEBCAM_ID")))
 
 win_name = "calibration_test_win"
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 cv2.setWindowProperty(win_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
-while cv2.waitKey(1) != ord('q'):
+while cv2.waitKey(1) != ord("q"):
     monitor_frame = np.zeros((monitor.height, monitor.width, 3), dtype=np.uint8)
 
     has_frame, frame = source.read()
@@ -31,8 +33,12 @@ while cv2.waitKey(1) != ord('q'):
     marker_corners, marker_ids, rejectedImgs = aruco_detector.detectMarkers(frame)
 
     for marker_corner_list in marker_corners:
-        points = cv2.perspectiveTransform(marker_corner_list.reshape(-1, 1, 2), camera_to_monitor)
-        monitor_frame = cv2.fillPoly(monitor_frame, [points.astype(np.uint8)], (0, 255, 255))
+        points = cv2.perspectiveTransform(
+            marker_corner_list.reshape(-1, 1, 2), camera_to_monitor
+        )
+        monitor_frame = cv2.fillPoly(
+            monitor_frame, [points.astype(np.uint8)], (0, 255, 255)
+        )
 
     cv2.imshow(win_name, monitor_frame)
 
