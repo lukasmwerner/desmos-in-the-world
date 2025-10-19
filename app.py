9af70bef08db0f6b0863aff488cf2295fc026134
Kylo33 <56988649+Kylo33@users.noreply.github.com>
2025-10-18 22:09:04 -0600

    fix bug in app

--- app.py
+++ app.py
@@ -64,8 +64,7 @@ def create_component(id, box, frame):
     category = id // 200
     if category == 0:
         # Graph
-        return component = components.GraphComponent(id, box, frame, [], sympy.abc.x ** 2)
-        # component.render(canvas_bgr, camera_to_monitor)
+        return components.GraphComponent(id, box, frame, [], sympy.abc.x**2)
     elif category == 1:
         # Equations
         return components.EquationComponent(id, box, frame)
