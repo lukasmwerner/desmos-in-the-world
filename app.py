c8ca56da61a4e950de896a10f4dd01d0e4c6a53b
b-sharman <byron.n.sharman@gmail.com>
2025-10-18 18:00:58 -0700

    use webcam instead of static image

--- app.py
+++ app.py
@@ -47,17 +47,16 @@ dict_key = cv2.aruco.DICT_4X4_1000
 aruco_dict = cv2.aruco.getPredefinedDictionary(dict_key)
 aruco_detector = cv2.aruco.ArucoDetector(aruco_dict)
 
-source = cv2.imread("20251018_170103.jpg")
-frame = source.copy()
-blank_frame = np.zeros_like(frame)
+source = cv2.VideoCapture(int(os.getenv("WEBCAM_ID")))
 win_name = "desmos-irl"
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 cv2.setWindowProperty(win_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
 while cv2.waitKey(1) != ord('q'):
-    # has_frame, frame = source.read()
-    # if not has_frame:
-    #     break
+    has_frame, frame = source.read()
+    blank_frame = np.zeros_like(frame)
+    if not has_frame:
+        break
 
     marker_corners, marker_ids, rejectedImgs = aruco_detector.detectMarkers(frame)
     # detected_marker_img = cv2.aruco.drawDetectedMarkers(frame, marker_corners, marker_ids)
@@ -74,11 +73,12 @@ while cv2.waitKey(1) != ord('q'):
 
     for box in boxes:
         cv2.polylines(
-            source,
+            frame,
             box.inner_coordinates().reshape((1, -1, 1, 2)),
             True,
             (255, 255, 0),
             20,
         )
 
-    cv2.imshow(win_name, source)
+source.release()
+cv2.destroyAllWindows()
