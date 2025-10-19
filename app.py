62dbd81a7ce0371c70544ddb50d2876b30558527
b-sharman <byron.n.sharman@gmail.com>
2025-10-19 05:02:43 -0700

    hopefully don't spam Gemini messages any more

--- app.py
+++ app.py
@@ -108,8 +108,11 @@ async def process_components(components, blank_frame, camera_to_monitor, connect
     q = deque()
 
     for component_id in components:
-        if isinstance(components[component_id], EquationComponent):
-            task = asyncio.create_task(components[component_id].compute_content())
+        if (
+            isinstance(components[component_id], EquationComponent)
+            and components[component_id].task is None
+        ):
+            components[component_id].task = asyncio.create_task(components[component_id].compute_content())
         elif isinstance(components[component_id], GraphComponent):
             components[component_id].inputs.clear()
         if indegrees[component_id] == 0:
