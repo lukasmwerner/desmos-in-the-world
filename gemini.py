27f2a1f1726bf3878b0225d295ce575aecd1b75b
Daghassi <danial.aghassi@gmail.com>
2025-10-18 20:27:13 -0600

    fixing cache of sympy objects

--- gemini.py
+++ gemini.py
@@ -20,15 +20,13 @@ def gemini(client, tags_list, tag_number, image):
         'ONLY PROVIDE THE SYMPY STRING REQUESTED, DO NOT INCLUDE QUATATIONS. THIS IS NOT A PYTHON SCRIPT DO NOT WRITE ANY PYTHON EVER. Given this image, generate a string of the equation provided in sympy format in order for sympy to underestand in the context of a function call for evaluating a mathematical. Example: (sin(x) - 2*cos(y)**2 + 3*tan(z)**3)**20)'
         ]
     ).text)
-    print(eqn)
-    print(type(eqn))
     tags_list[tag_number] = eqn
     return eqn
 
 if __name__ == "__main__":
     load_dotenv()
     client = make_gemini_client()
-    tags_list = [None for i in range(200,399)]
-    print(gemini(client, tags_list, 100, "image.png"))
-    print(gemini(client, tags_list, 1, "image copy.png"))
-    print(tags_list)
+    tags_dict = {}
+    for i in range(200, 400):
+        tags_dict[i] = None
+    print(tags_dict)
